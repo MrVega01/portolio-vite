606773d7637b695e04673db3a12a85a29d0bbd66
MrVega01 <ignaciovega2003@outlook.com>
2023-08-14 02:21:22 -0400

    Add waves separator

---
+++ src/components/Separators/Separators.jsx
@@ -0,0 +1,40 @@
+import { useId } from 'react'
+import './styles.css'
+
+export function WavesSeparator ({ colors = ['#fff', '#f0f', '#ff0', '#1ff'], background = '#000' }) {
+  const pathId = useId()
+  return (
+    <svg
+      className='waveSeparator'
+      xmlns='http://www.w3.org/2000/svg'
+      xmlnsXlink='http://www.w3.org/1999/xlink'
+      viewBox='0 24 150 28 '
+      preserveAspectRatio='none'
+      style={{ background }}
+    >
+      <defs>
+        <path
+          id={pathId}
+          d='M-160 44c30 0
+    58-18 88-18s
+    58 18 88 18
+    58-18 88-18
+    58 18 88 18
+    v44h-352z'
+        />
+      </defs>
+      <g className='wave1'>
+        <use xlinkHref={`#${pathId}`} x='50' y='3' fill={colors[0]} />
+      </g>
+      <g className='wave2'>
+        <use xlinkHref={`#${pathId}`} x='50' y='0' fill={colors[1]} />
+      </g>
+      <g className='wave3'>
+        <use xlinkHref={`#${pathId}`} x='50' y='9' fill={colors[2]} />
+      </g>
+      <g className='wave4'>
+        <use xlinkHref={`#${pathId}`} x='50' y='6' fill={colors[3]} />
+      </g>
+    </svg>
+  )
+}
