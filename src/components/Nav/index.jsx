caa6ca734b12e899c3b85acb7059a8f8e09eb5a6
MrVega01 <ignaciovega2003@outlook.com>
2023-08-19 02:52:54 -0400

    Add Nav component

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,9 +1,20 @@
+import { NavLink } from 'react-router-dom'
+import { MainIcon } from '../Icons'
 import './styles.css'
 
 export default function Nav () {
   return (
-    <div className='navContainer'>
-      <div className='navBar' />
-    </div>
+    <nav>
+      <a href='/'>
+        <MainIcon />
+      </a>
+      <ul>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/experience'>Experience</NavLink></li>
+        <li><NavLink to='/skills'>Skills</NavLink></li>
+        <li><NavLink to='/projects'>Projects</NavLink></li>
+        <li><NavLink to='/contact'>Contact</NavLink></li>
+      </ul>
+    </nav>
   )
 }
