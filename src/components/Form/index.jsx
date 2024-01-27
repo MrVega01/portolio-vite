80be5c128079bce36b7e90e6fad6013a8aff28f3
MrVega01 <ignaciovega2003@outlook.com>
2024-01-27 02:41:14 -0400

    Add loading style in form and reset after submit

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,18 +1,30 @@
+import { useState, useCallback } from 'react'
 import './styles.css'
 
 export default function Form ({ children, textSubmit = 'Submit', onSubmit, className, ...restOfProps }) {
-  const submitHandler = (event) => {
+  const [loading, setLoading] = useState(false)
+
+  const submitHandler = useCallback(async (event) => {
     event.preventDefault()
-    const formData = Object.fromEntries(
-      new FormData(event.target)
-    )
-    onSubmit && onSubmit(formData)
-  }
+    try {
+      setLoading(true)
+      const formData = Object.fromEntries(
+        new FormData(event.target)
+      )
+      onSubmit && await onSubmit(formData)
+    } catch (e) {
+    } finally {
+      event.target.reset()
+      setLoading(false)
+    }
+  }, [])
 
   return (
     <form className={`Form ${className}`} onSubmit={submitHandler} {...restOfProps}>
       {children}
-      <input type='submit' value={textSubmit} />
+      <button type='submit'>
+        {loading ? <span className='loading'>⏳</span> : textSubmit}
+      </button>
     </form>
   )
 }
